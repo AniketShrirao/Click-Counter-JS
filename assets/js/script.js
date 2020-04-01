494ca454cf19f04af0a35eb323fa8a829d95396d
saniketprdxn <anikets@prdxn.com>
2020-04-01 12:46:58 +0530

    js Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
 /* Author: Aniket*/
+// Selecting Required Buttons to Count
+const buttons = Array.from(document.querySelectorAll('button'));
+let countContainer = document.querySelector('.counter p');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+buttons.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		if(e.target.classList.contains('add-count')) {
+			countContainer.innerText++;
+		} else if(e.target.classList.contains('lower-count')) {
+			if(countContainer.innerText == 0) {
+				e.preventDefault();
+			} else {
+				countContainer.innerText --;
+			}
+		}
+})
+})
